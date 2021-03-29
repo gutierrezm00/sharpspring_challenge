ae108afc8edd888dc7b1ef208ab259265b7bda8c
Mathew Gutierrez <mathew@primelabs.ms>
2021-03-28 22:58:47 -0600

    sqlite -> pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_28_174608) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.string "body"
